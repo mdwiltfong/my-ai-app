3b53f8921d228b1e3ca850690cef3754c943c68c
James <jkeezer@pm.me>
2023-11-13 17:55:32 -0500

    Tests #2 (renders input) and #3 (type in input)

--- __tests__/page.test.js
+++ __tests__/page.test.js
@@ -1,5 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 import App from '@/app/page';
 
 describe('App', () => {
@@ -7,4 +8,27 @@ describe('App', () => {
     render(<App />);
     expect(screen.getByRole('form')).toBeInTheDocument();
   });
-})
+
+  it('renders an input', () => {
+    render(<App />);
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
+  });
+
+  it('allows the user to type into the input', () => {
+    render(<App />);
+
+    const inputElement = screen.getByRole('textbox');
+    fireEvent.change(inputElement, { target: { value: 'Hello, World!' } });
+    // userEvent is a more realistic way to test than fireEvent, but doesn't seem to work here
+    // userEvent.type(inputElement, 'Hello, World!');
+    console.log(inputElement.value)
+    expect(inputElement.value).toEqual('Hello, World!');
+  });
+});
+
+// describe('App', () => {
+//   it('renders a button', () => {
+//     render(<App />);
+//     expect(screen.getByRole('button')).toBeInTheDocument();
+//   });
+// });
