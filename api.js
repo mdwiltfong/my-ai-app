7dd109d876390f4922f39180636ec1606fa4c953
Michael Wiltfong <76107997+mdwiltfong@users.noreply.github.com>
2023-11-13 18:05:51 -0500

    updating npm package and client so that it connects with `doc-inspector`

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-import { Client } from "@gadget-client/my-ai-app-mdw";
+import { Client } from "@gadget-client/doc-inspector";
 export const api = new Client({
   // for client side data access, we don't pass anything and the client will default to using the browser session authentication mode
   // for server side data access, pass an API key by uncommenting the line below
