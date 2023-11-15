fff85755f10dc95a95a099f2ccd5506da14bfe93
James <jkeezer@pm.me>
2023-11-14 22:36:39 -0500

    Reusable FileCard for PDF & MD

--- app/components/FileUpload.js
+++ app/components/FileUpload.js
@@ -5,10 +5,11 @@ import api from '../../api';
 import { Button, Typography } from '@mui/material';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
-function FileUpload() {
+function FileUpload({type}) {
   const [file, setFile] = useState('');
   const [{ data, error, fetching }, create] = useAction(api.document.create);
-
+  const filetype = type === 'pdf' ? '.pdf' : '.md';
+  
   const handleFileChange = (e) => {
     if (e.target.files) {
       setFile(e.target.files[0]);
@@ -33,8 +34,8 @@ function FileUpload() {
 
   return (
     <div className='flex flex-col gap-2 mb-4'>
-      <Typography>Upload a PDF</Typography>
-      <input type='file' accept='.pdf' onChange={handleFileChange} />
+      <Typography>Upload a {type} file</Typography>
+      <input type='file' accept={filetype} onChange={handleFileChange} />
 
       <div>{file && `${file.name} - ${file.type}`}</div>
 
--- app/page.js
+++ app/page.js
@@ -8,12 +8,19 @@ export default function DocInspector() {
   const { completion, input, handleInputChange, handleSubmit } =
     useCompletion();
 
+  const FileCard = ({ type }) => (
+    <Card sx={{ padding: 3, backgroundColor: '#DDD', flexGrow: 0 }}>
+      <FileUpload type={type} />
+    </Card>
+  );
+
   return (
-    <div className='mx-auto w-full max-w-md py-24 gap-4 flex flex-col stretch'>
-      <Card sx={{ padding: 3, backgroundColor: "#DDD" }}>
-        <FileUpload />
-      </Card>
-      <form onSubmit={handleSubmit}>
+    <div className='mx-auto w-full max-w-3xl py-24 gap-4 flex flex-col stretch items-center'>
+      <div className='flex flex-row flex-wrap gap-4 justify-center'>
+        <FileCard type='pdf' />
+        <FileCard type='md' />
+      </div>
+      <form onSubmit={handleSubmit} className="w-full flex justify-center">
         <input
           className='fixed w-full max-w-md bottom-0 border border-gray-300 rounded mb-8 shadow-xl p-2 dark:text-black'
           value={input}
