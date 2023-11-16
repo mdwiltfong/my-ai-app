e9091e053d9d436d09f9aca239176e379c70d7f2
James <jkeezer@pm.me>
2023-11-15 22:49:36 -0500

    Removing apiKey auth mode

--- api.js
+++ api.js
@@ -2,6 +2,6 @@ import { Client } from "@gadget-client/doc-inspector";
 export const api = new Client({
   // for client side data access, we don't pass anything and the client will default to using the browser session authentication mode
   // for server side data access, pass an API key by uncommenting the line below
-  authenticationMode: { apiKey: process.env.GADGET_API_KEY },
+  // authenticationMode: { apiKey: process.env.GADGET_API_KEY },
 });
 
