92ada2f656e45031744a5b42a417cf68edc2cf6f
Michael Wiltfong <76107997+mdwiltfong@users.noreply.github.com>
2023-11-16 19:30:57 -0500

    anonymous session and npm test script

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
-import { Client } from '@gadget-client/doc-inspector';
+import { Client } from "@gadget-client/doc-inspector";
 export const api = new Client({
-  environment: 'Development',
+  environment: "Development",
   // for client side data access, we don't pass anything and the client will default to using the browser session authentication mode
   // for server side data access, pass an API key by uncommenting the line below
-  // authenticationMode: { apiKey: process.env.GADGET_API_KEY },
+  authenticationMode: { anonymous: true },
 });
