245ce372a2211c5213582baf30558cd5c374d71a
James <jkeezer@pm.me>
2023-11-16 23:47:33 -0500

    UI improvements & remove dark mode for now

--- app/components/FileUpload.js
+++ app/components/FileUpload.js
@@ -4,8 +4,9 @@ import { useAction } from '@gadgetinc/react';
 import { api } from '../../api';
 import { Button, Typography } from '@mui/material';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { styled } from '@mui/material/styles';
 
-function FileUpload({type}) {
+function FileUpload({ type }) {
   const [file, setFile] = useState('');
   const [{ data, error, fetching }, create] = useAction(api.document.create);
   const filetype = type === 'pdf' ? '.pdf' : '.md';
@@ -32,14 +33,57 @@ function FileUpload({type}) {
     }
   };
 
+  const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+  });
+
   return (
-    <div className='flex flex-col gap-2 mb-4'>
+    <div className='flex flex-col gap-2 mb-4 items-center'>
       <Typography>Upload a {type} file</Typography>
-      <input type='file' accept={filetype} onChange={handleFileChange} />
+      {/* <input type='file' accept={filetype} onChange={handleFileChange} /> */}
 
-      <div>{file && `${file.name} - ${file.type}`}</div>
-
-      <Button component="label" variant="contained" className="bg-indigo-900 hover:bg-indigo-800" startIcon={<CloudUploadIcon />} onClick={handleUploadClick}>Upload File</Button>
+      <Button
+        component='label'
+        variant='contained'
+        startIcon={<CloudUploadIcon />}
+      >
+        {file === '' ? 'Upload' : 'Change'} file
+        <VisuallyHiddenInput
+          type='file'
+          accept={filetype}
+          onChange={handleFileChange}
+        />
+      </Button>
+      <div className='flex flex-row flex-nowrap my-2 w-full justify-between items-center'>
+        <p>{file && file.name}</p>{' '}
+        {file && (
+          <a
+            className='cursor-pointer font-bold text-red-800 py-1 px-2 mx-2'
+            onClick={() => {
+              setFile('');
+            }}
+          >
+            X
+          </a>
+        )}
+      </div>
+      <Button
+        component='label'
+        variant='contained'
+        disabled={file === ''}
+        className='bg-indigo-900 hover:bg-indigo-800'
+        onClick={handleUploadClick}
+      >
+        Upload
+      </Button>
       {data && <p>Uploaded: {data.filename}</p>}
     </div>
   );
--- app/page.js
+++ app/page.js
@@ -9,32 +9,40 @@ export default function App() {
     useCompletion();
 
   const FileCard = ({ type }) => (
-    <Card sx={{ padding: 3, backgroundColor: '#DDD', flexGrow: 0 }}>
+    <Card sx={{ padding: 3, backgroundColor: '#FCFCFC', flexGrow: 0 }}>
       <FileUpload type={type} />
     </Card>
   );
 
   return (
-    <div className='mx-auto w-full max-w-3xl py-24 gap-4 flex flex-col stretch items-center'>
-      <div className='flex flex-row flex-wrap gap-4 justify-center'>
+    <div className='flex flex-col stretch items-center mx-auto w-full min-h-screen max-w-3xl py-24 gap-4'>
+      <div className='grid sm:grid-cols-2 gap-4 w-full'>
         <FileCard type='pdf' />
         <FileCard type='md' />
       </div>
-    <form role="form" onSubmit={handleSubmit} className="w-full flex justify-center">
+      {completion ? (
+        <div className='whitespace-pre-wrap my-4 content-end align-bottom grow flex flex-col justify-end align-start w-full bg-gray-100 rounded-lg p-4 border border-gray-300'>
+          {completion}
+        </div>
+      ) : (
+        <div className='whitespace-pre-wrap my-4 content-end align-bottom grow flex flex-col justify-end align-start w-full bg-gray-100 rounded-lg p-4 border border-gray-300'>
+          <p>
+            Upload a PDF document & Markdown file to begin
+          </p>
+        </div>
+      )}
+      <form
+        role='form'
+        onSubmit={handleSubmit}
+        className='fixed bottom-0 w-full flex justify-center'
+      >
         <input
           className='fixed w-full max-w-md bottom-0 border border-gray-300 rounded mb-8 shadow-xl p-2 dark:text-black'
           value={input}
-          placeholder='Describe your business...'
+          placeholder='Ask a question...'
           onChange={handleInputChange}
         />
       </form>
-      {completion ? (
-        <div className='whitespace-pre-wrap my-4'>{completion}</div>
-      ) : (
-        <div>
-          Enter a business description and click enter to generate slogans.
-        </div>
-      )}
     </div>
   );
 }
